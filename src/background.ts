fb56dc686dde5896bee06d4b10c81c3f43d6c556
user <user@example.com>
2022-03-27 15:38:28 +0200

    merge old code

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,33 @@
+import OnClickData = chrome.contextMenus.OnClickData;
+
 function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
+    console.log("polling");
+    setTimeout(polling, 1000 * 30);
 }
 
 polling();
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('installed')
+});
+
+function getword(info: OnClickData, tab?: chrome.tabs.Tab) {
+    if (info.menuItemId !== CONTEXT_MENU_ID) {
+        return;
+    }
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs: chrome.tabs.Tab[]) {
+        chrome.tabs.sendMessage(<number>tabs[0].id, {selected: info.selectionText}, function (response) {
+            // console.log(response.farewell);
+        });
+    });
+}
+
+const CONTEXT_MENU_ID = "MY_CONTEXT_MENU";
+chrome.contextMenus.create({
+    title: "hello world",
+    contexts: ["selection"],
+    id: CONTEXT_MENU_ID,
+});
+chrome.contextMenus.onClicked.addListener(
+    getword
+);
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,43 @@
+import {Diff, diffChars} from 'diff'
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+    if (msg.color) {
+        console.log("Receive color = " + msg.color);
+        document.body.style.backgroundColor = msg.color;
+        sendResponse("Change color to " + msg.color);
+    } else {
+        sendResponse("Color message is none.");
+    }
 });
+
+
+console.log('hello world!')
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log('request', request)
+        navigator.clipboard.readText().then(
+            clipText => {
+                console.log('selected', request.selected)
+                console.log('clipText', clipText)
+                let selection = window.getSelection()?.getRangeAt(0);
+                let selectedText = selection?.extractContents();
+                let span = document.createElement("span");
+                span.style.color = "#ff8181"
+                span.appendChild(selectedText!)
+                selection?.insertNode(span)
+
+                const diff = diffChars(request.selected, clipText);
+
+                diff.forEach((part:any) => {
+                    // green for additions, red for deletions
+                    // grey for common parts
+                    // const color = part.added ? 'green' :
+                    //     part.removed ? 'red' : 'grey';
+                    // process.stderr.write(part.value[color]);
+                    console.log('part(' + part.added + ')', part)
+                });
+            }
+        )
+    }
+);
